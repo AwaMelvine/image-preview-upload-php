4e3c1ed8eac2133d7274db8652c562597030b52b
Awa Melvine <melvineawa@gmail.com>
2018-07-23 22:43:40 -0700

    Displaying 'Update Image' on hover over image placeholder

--- form.php
+++ form.php
@@ -9,12 +9,7 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
-  <style>
-    .form-div {
-      margin-top: 100px;
-      border: 1px solid #e0e0e0;
-    }
-  </style>
+  <link rel="stylesheet" href="main.css">
 </head>
 <body>
 
@@ -26,20 +21,22 @@
           <h2 class="text-center mb-3 mt-3">Update profile</h2>
 
           <?php if (!empty($msg)): ?>
-            <div class="alert alert-primary" role="alert">
+            <div class="alert <?php echo $msg_class ?>" role="alert">
               <?php echo $msg; ?>
             </div>
           <?php endif; ?>
 
-          <?php if (!empty($error)): ?>
-            <div class="alert alert-danger" role="alert">
-              <?php echo $error; ?>
-            </div>
-          <?php endif; ?>
+          <div class="form-group text-center" style="position: relative;" >
 
-          <div class="form-group">
+            <span class="img-div">
+              <div class="text-center img-placeholder"  onClick="triggerClick()">
+                <h4>Update image</h4>
+              </div>
+              <img src="images/avatar.jpg" onClick="triggerClick()" id="profileDisplay">
+            </span>
+
+            <input type="file" name="profileImage" onChange="displayImage(this)" id="profileImage" class="form-control" style="display: none;">
             <label>Profile Image</label>
-            <input type="file" name="profileImage" class="form-control">
           </div>
           <div class="form-group">
             <label>Bio</label>
@@ -54,6 +51,24 @@
     </div>
   </div>
 
-
 </body>
 </html>
+
+<script type="text/javascript">
+  function triggerClick(e) {
+    document.querySelector('#profileImage').click();
+  }
+
+  function displayImage(e) {
+    if (e.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e){
+        document.querySelector('#profileDisplay').setAttribute('src', e.target.result);
+      }
+      reader.readAsDataURL(e.files[0]);
+    }
+  }
+
+
+
+</script>
--- processForm.php
+++ processForm.php
@@ -1,34 +1,37 @@
 <?php
   $msg = "";
-  $error = "";
+  $msg_class = "";
 
   if (isset($_POST['save_profile'])) {
     // for the database
     $bio = stripslashes($_POST['bio']);
-    $profileImageName = date("Y-m-d H:i:s") . '-' . $_FILES["profileImage"]["name"];
+    $profileImageName = time() . '-' . $_FILES["profileImage"]["name"];
 
     // For image upload
     $target_dir = "images/";
-    $target_file = $target_dir . $profileImageName;
-
+    $target_file = $target_dir . basename($profileImageName);
 
     // VALIDATION
     // validate image size. Size is calculated in Bytes
     if($_FILES['profileImage']['size'] > 200000) {
-      $error = "Image size should not be greated than 200Kb";
+      $msg = "Image size should not be greated than 200Kb";
+      $msg_class = "alert-danger";
     }
 
     // check if file exists
     if(file_exists($target_file)) {
-      $error = "File already exists";
+      $msg = "File already exists";
+      $msg_class = "alert-danger";
     }
 
     // Upload image only if no errors
     if (empty($error)) {
       if(move_uploaded_file($_FILES["profileImage"]["tmp_name"], $target_file)) {
         $msg = "The file has been uploaded";
+        $msg_class = "alert-success";
       } else {
         $error = "There was an erro uploading the file";
+        $msg = "alert-danger";
       }
     }
 
